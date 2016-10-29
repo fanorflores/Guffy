0ff3462c521f6c562bd9988d255d55e5ddd41184
FANOR\Felipe <fanorflores@gmail.com>
2016-10-29 16:34:42 -0600

    Primera Entrega

---
+++ app/src/main/java/ni/edu/unan/te/guffy/MainActivity.java
@@ -0,0 +1,13 @@
+package ni.edu.unan.te.guffy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
